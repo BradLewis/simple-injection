1b4c70f20932d7c5d4775d25f4fb496df4036458
Bradlewis <22850972+BradLewis@users.noreply.github.com>
2020-09-11 02:45:15 -0400

    Add typings

--- dependency_injection/service_collection.py
+++ dependency_injection/service_collection.py
@@ -1,4 +1,7 @@
 from enum import Enum, auto
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
+
+T = TypeVar("T")
 
 
 class ServiceLifetime(Enum):
@@ -8,18 +11,29 @@ class ServiceLifetime(Enum):
 
 
 class ContainerService:
-    def __init__(self, service_implementation, service_lifetime, args=None):
+    def __init__(
+        self,
+        service_implementation: Union[Type[T], T],
+        service_lifetime: ServiceLifetime,
+        args: Optional[List[Any]] = None,
+    ):
         self.service_implementation = service_implementation
         self.service_lifetime = service_lifetime
         self.args = args
-        self.instance = None
+        self.instance: T = None
 
 
 class ServiceCollection:
     def __init__(self):
-        self._service_collection = dict()
+        self._service_collection: Dict[str, ContainerService] = dict()
 
-    def add(self, service_to_add, service_implementation, service_lifetime, args=None):
+    def add(
+        self,
+        service_to_add: Type[T],
+        service_implementation: Union[T, Type[T]],
+        service_lifetime: ServiceLifetime,
+        args: Optional[List[Any]] = None,
+    ):
         if service_implementation is None:
             service_implementation = service_to_add
 
@@ -27,13 +41,23 @@ class ServiceCollection:
             service_implementation, service_lifetime, args
         )
 
-    def add_transient(self, service_to_add, service_implementation=None, args=None):
-        self.add(service_to_add, service_implementation, ServiceLifetime.TRANSIENT, args)
+    def add_transient(
+        self,
+        service_to_add: Type[T],
+        service_implementation: Optional[Type[T]] = None,
+        args: Optional[List[Any]] = None,
+    ):
+        self.add(
+            service_to_add, service_implementation, ServiceLifetime.TRANSIENT, args
+        )
 
-    def resolve(self, service_to_resolve):
+    def resolve(self, service_to_resolve: Type[T]):
         container_service = self._service_collection[service_to_resolve.__name__]
         if container_service.args:
             return container_service.service_implementation(*container_service.args)
+        return self._resolve_transient(container_service)
+
+    def _resolve_transient(self, container_service: ContainerService):
         annotations = container_service.__init__.__annotations__
         services_to_use = list()
         for annotation in annotations.values():
