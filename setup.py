d60488865e101f2f6b4fd078ab6dcc83cd8b4493
Bradlewis <22850972+BradLewis@users.noreply.github.com>
2020-09-14 12:48:02 -0400

    Fixing black check in pipeline

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="simple-injection",
-    version="0.2.0",
+    version="0.2.0dev",
     description="A simple library for typing-based dependency injection",
     author="Bradley Lewis",
     license="MIT",
