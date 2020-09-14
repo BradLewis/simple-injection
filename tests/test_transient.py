d44e0d97ed28cf9e2dd6607babc50e87451eecc9
Bradlewis <22850972+BradLewis@users.noreply.github.com>
2020-09-13 21:11:33 -0400

    Adding test with args

--- tests/test_transient.py
+++ tests/test_transient.py
@@ -1,5 +1,5 @@
-from simple_injection.service_collection import ServiceCollection
-from tests.classes import A, B, C
+from simple_injection import ServiceCollection, ServiceResolverFlags
+from tests.classes import A, B, C, NoTyping, SomeTyping
 
 
 def test_all_transient():
@@ -15,3 +15,15 @@ def test_all_transient():
     assert a != b.a
     assert b != c.b
     assert a != c.b.a
+
+
+def test_with_args():
+    collection = ServiceCollection()
+    collection.add_transient(NoTyping, args=["a", "b"])
+    collection.add_transient(
+        SomeTyping, args=[ServiceResolverFlags.REQUIRED_SERVICE, "b"]
+    )
+
+    some_typing = collection.resolve(SomeTyping)
+    no_typing = collection.resolve(NoTyping)
+    assert some_typing._no_typing != no_typing
